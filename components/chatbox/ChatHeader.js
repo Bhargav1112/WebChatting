b385eaa1ca8fde4a7e03b9358ea6191a0c72de30
RushabhAntada1010 <mr.rushabhantala1010@gmail.com>
2022-12-07 21:15:22 +0530

    Chat header design set

--- components/chatbox/ChatHeader.js
+++ components/chatbox/ChatHeader.js
@@ -1,10 +1,34 @@
+import { Avatar } from '@mui/material'
 import React from 'react'
+import styles from '../chatList/style.module.scss'
+import { IoVideocamOutline } from "react-icons/io5";
+import { RiPhoneLine } from 'react-icons/ri';
+import { FiMoreVertical } from 'react-icons/fi';
+import { useState } from 'react';
 
 const ChatHeader = () => {
+  const [open, setOpen] = useState(false)
+  const handleClick = () => {
+    setOpen(true)
+  }
   return (
-    <div>
-      ChatHeader
-      ChatHeader
+    <div className={styles.ChatHeaderWrapper}>
+      <div className={styles.ChatHeader}>
+        <div className={styles.ChatLogo}>
+          <Avatar />
+          <div className={styles.HeaderDetails}>
+            <h5>Rushabh Antala</h5>
+            <p>Online</p>
+          </div>
+        </div>
+        <div className={styles.HeaderFeature}>
+          <IoVideocamOutline />
+          <RiPhoneLine />
+          <a href='js:' onClick={() => handleClick()}>
+            <FiMoreVertical />
+          </a>
+        </div>
+      </div>
     </div>
   )
 }
---
+++ firebase/auth.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import firebase from 'firebase/compat/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+    apiKey: "AIzaSyCLdLYMLDqqY4bHk40y52CyWrtzP0llpxM",
+    authDomain: "chat-web-5f854.firebaseapp.com",
+    projectId: "chat-web-5f854",
+    storageBucket: "chat-web-5f854.appspot.com",
+    messagingSenderId: "160058367127",
+    appId: "1:160058367127:web:bcfb4e15d5d2624ccd1dd2",
+    measurementId: "G-L3HTJS01K9"
+};
+
+// Initialize Firebase
